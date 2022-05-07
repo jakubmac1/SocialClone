b2e4580d369f162aae1e71cc1637ce496092934e
Jakub Maciasz <maciasz.jakub@gmail.com>
2022-05-07 19:21:35 +0200

    whitenoise

--- social_clone/settings.py
+++ social_clone/settings.py
@@ -54,8 +54,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'social_clone.urls'
 
 TEMPLATES = [
