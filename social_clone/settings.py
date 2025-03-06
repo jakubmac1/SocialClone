7b8553e2fe5953686425d36be5e234800c78349d
Jakub Maciasz <57491280+jakubmac1@users.noreply.github.com>
2025-03-06 13:35:36 +0100

    Update settings.py

--- social_clone/settings.py
+++ social_clone/settings.py
@@ -21,7 +21,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fm-@mpyo_6&3slb)1iet*zeu+xe&8f4e^k^g*!^d@)57yxm)0g'
+SECRET_KEY = '###'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
